83d44ad70598df9a1ea1ac46f5b1e1eed14c9cb7
zhaoruibing <632713700@qq.com>
2018-09-11 21:00:21 +0800

    Merge branch 'master' of https://github.com/divineiu/test
    # Conflicts:
    #	new/index.php

--- new/index.php
+++ new/index.php
@@ -3,7 +3,6 @@
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2018/9/11
-<<<<<<< HEAD
  * Time: 20:23
  */
 
@@ -13,16 +12,4 @@ class index
     {
         echo 111;
     }
-
-=======
- * Time: 20:10
- */
-class index
-{
-    public function test1()
-    {
-        echo 'test1';
-        echo 'test1';
-    }
->>>>>>> 714775d4640e17a3f5cd4b6d28494952e762dd53
 }
