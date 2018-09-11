cb0480ee72d5e6888c6f5e4b8cfa1af72771e776
zhaoruibing <632713700@qq.com>
2018-09-11 20:51:11 +0800

    2222

---
+++ new/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/11
+ * Time: 20:23
+ */
+
+class index
+{
+    public function index()
+    {
+        echo 111;
+    }
+
+}
