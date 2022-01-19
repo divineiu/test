057546255ccd16b7b07045222b68cd2b75dfc5af
Longines <632713700@qq.com>
2022-01-20 00:29:25 +0800

    请问

---
+++ new/new12.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/11
+ * Time: 20:10
+ */
+class news
+{
+    public function test1()
+    {
+        echo 'test1';
+        echo 'test1';
+        echo 'test1';
+        echo 'test1';
+    }
+    public function sss()
+    {
+
+    }
+
+}
